7c34a1d2fe59b4f0fd1850615455ed8d5a926897
ferru03 <76011910+ferru03@users.noreply.github.com>
2022-06-05 16:39:58 +0200

    Update index.php

--- mioSitoDatatable-main/mio-sito/index.php
+++ mioSitoDatatable-main/mio-sito/index.php
@@ -1,5 +1,5 @@
 <?php
-    include "pages/datalayer.php";
+    include "pages/dataLayer.php";
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
